0fe43cea76baf08c80d4147a772e7b40ba8af501
Diego Carvajal <df.carvajal@uniandes.edu.co>
2021-10-01 14:27:52 -0500

    Parcial 1

--- public/script.js
+++ public/script.js
@@ -4,11 +4,27 @@ const tableSection = document.getElementById('table-section');
 const table = document.getElementById('table');
 const tbody = document.getElementById('body-table');
 const message = document.getElementById('message');
+const express = require("express");
+const app = express();
+let url = `https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players`;
+
+function callApi() {
+  fetch(
+    url
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log('data', data);
+      render(data.data);
+    });
+}
 
 enterButton.addEventListener('click', (event) => {
   //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+  const input = document.getElementById('inputText');
+
+  //alert('Implementar lógica del button submit');
+  getresults(input);
   event.preventDefault();
 });
 
@@ -20,7 +36,7 @@ async function getresults(heightRef) {
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
   console.log('data from back', data);
-  //printValues(data);
+  printValues(data);
 }
 
 function removeAllChildNodes(parent) {
@@ -28,3 +44,7 @@ function removeAllChildNodes(parent) {
     parent.removeChild(parent.firstChild);
   }
 }
+
+(function init(){
+  callApi();
+})();
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,37 @@
 const { response, request } = require('express');
+const res = require('express/lib/response');
+const axios = require('axios');
+const url = 'https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players';
 
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
   //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  const alturaTotal = (pareja) => {
+    return pareja.reduce((previo,actual) =>
+      previo.h_in+actual.h_in);  
+  }
+  const findParejas = (arreglo,altura) =>{
+    let rta = [];
+    arreglo.forEach(atleta1=> { 
+      arreglo.forEach(atleta2 =>{
+        let pareja = [atleta1,atleta2];
+        if (alturaTotal(pareja)===altura){
+          rta.push(pareja)
+        }});
+      })
+    return rta;
+  }
+
+  let array = [];
+  axios.get(url).then(resp => {
+    array = resp.data.values;
+    let answer = findParejas(array,req.params.altura);
+    console.log(answer);
+  })
+
+  return resp.json(array);
 };
+
+
+
 
 module.exports = { getPairsOfPlayers };
